b62d4dc56ae74f144d0fb0ba2aaf70f0ee5b3d6d
leandro-moraes <leandromoraes.developer@gmail.com>
2026-02-17 20:37:08 -0300

    feat: add findById method to IVideosRepository and implement in FakeVideosRepository

--- __tests__/fakes/repositories/fake-videos-repository.ts
+++ __tests__/fakes/repositories/fake-videos-repository.ts
@@ -1,12 +1,18 @@
 import { Video } from "../../../src/domain/enterprise/entities/video";
+import type { UniqueId } from "../../../src/domain/enterprise/object-value/unique-id";
 import type { IVideosRepository } from "../../../src/domain/enterprise/repositories/videos-repository";
 
 export class FakeVideosRepository implements IVideosRepository {
   public videos: Video[] = [];
 
-  async findAll(data: { limit: number; page: number }): Promise<Video[]> {
+  public async findAll(data: { limit: number; page: number }): Promise<Video[]> {
     const start = (data.page - 1) * data.limit;
     const end = start + data.limit;
     return this.videos.slice(start, end);
+  }
+
+  public async findById(id: UniqueId): Promise<Video | null> {
+    const video = this.videos.find((video) => video.id === id.toValue);
+    return Promise.resolve(video || null);
   }
 }
--- src/domain/enterprise/repositories/videos-repository.ts
+++ src/domain/enterprise/repositories/videos-repository.ts
@@ -1,6 +1,8 @@
 import type { IPaginationProps } from "../dtos/pagination-props";
 import type { Video } from "../entities/video";
+import type { UniqueId } from "../object-value/unique-id";
 
 export interface IVideosRepository {
+  findById(id: UniqueId): Promise<Video | null>;
   findAll(props: IPaginationProps): Promise<Video[]>;
 }
